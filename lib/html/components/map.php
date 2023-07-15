ac0a80e36db441fd5ec3fc63a7b2e227ba56eae9
Amauriams <amauri.ams@isgh.org.br>
2023-07-15 13:39:53 -0300

    Inicio

---
+++ lib/html/components/map.php
@@ -0,0 +1,48 @@
+<div class="containers">
+    <div class="rows">
+        <div class="coluna">
+            <div class="boxcoloroneimg">
+                <div class="content2 col col-md-6">
+                    <span class="spanmap">SIGA NOSSAS</span>
+                    <h1 class="titulomap">Redes Sociais</h1>
+                </div>
+            </div>
+            <div class="fb-post" 
+                data-href="https://www.facebook.com/20531316728/posts/10154009990506729/" 
+                data-width="auto" 
+                data-show-text="true">
+                <blockquote 
+                    cite="https://pt-br.facebook.com/20531316728/posts/10154009990506729/" 
+                    class="fb-xfbml-parse-ignore">
+                        Publicado por 
+                            <a href="https://www.facebook.com/facebook/">
+                                Facebook
+                            </a> 
+                            em&nbsp;
+                            <a href="https://pt-br.facebook.com/20531316728/posts/10154009990506729/">
+                                Quinta-feira, 27 de agosto de 2015
+                            </a>
+                </blockquote>
+            </div>
+        </div>
+        <div class="coluna">
+            <div class="boxcolortwoimg">
+                <div class="content2 col col-md-6">
+                    <span class="spanmap">COMO</span>
+                    <h1 class="titulomap">Chegar</h1>
+                </div>
+            </div>
+            <div>
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3980.898830493872!2d-38.48044308524066!3d-3.831881047212537!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74552fb96a3d1%3A0x4a5ccc137a41617!2sInstituto%20de%20Sa%C3%BAde%20e%20Gest%C3%A3o%20Hospitalar!5e0!3m2!1spt-BR!2sbr!4v1689094622838!5m2!1spt-BR!2sbr" 
+                    width="600" 
+                    height="600" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade" 
+                    style="margin: 0; border: 0; width: 100%" >
+                </iframe>
+            </div>
+        </div>
+    </div>
+</div>
